cf690f0ede393efd680b079be65941b6e6618eef
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-06 19:50:16 +0100

    enabled jack

--- config/home.nix
+++ config/home.nix
@@ -15,15 +15,3 @@
     secrets.wg-preshared-key = {};
   };
 }
-
-
-# Accessing the Secret as an Environment Variable
-
-# In your config you can use $(cat ${config.sops.secrets.openai_api_key.path}) to get the secret. For example I configure my zsh file to export the OPENAI_API_KEY.
-
-#  programs.zsh = {
-#     initExtra = ''
-#       # other config...
-#       export OPENAI_API_KEY=$(cat ${config.sops.secrets.openai_api_key.path})
-#     '';
-#   };

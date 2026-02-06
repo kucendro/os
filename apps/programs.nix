7d155b6f899e6909a2c27e4cd11241d050990971
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-06 22:56:11 +0100

    temporary rm hyprland and shell

--- apps/programs.nix
+++ apps/programs.nix
@@ -5,9 +5,9 @@
   programs.git.enable = true;
   programs.zsh.enable = true;
 
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
-  };
+  # programs.hyprland = {
+  #   enable = true;
+  #   withUWSM = true;
+  #   xwayland.enable = true;
+  # };
 }

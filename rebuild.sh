5e40b420b33545c9239d4e8dc902f8e59ee2a85f
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-06 15:27:26 +0100

    updated script for advanced functionality

--- rebuild.sh
+++ rebuild.sh
@@ -3,12 +3,27 @@ set -e
 
 cd "$(dirname "$0")"
 
+echo "---------- Testing build ----------"
+
+build_output=$(sudo nixos-rebuild build --flake . 2>&1) || { echo "$build_output"; echo "Build failed!"; exit 1; }
+
+echo "OK"
+echo "-----------------------------------"
+echo "$build_output"
+echo "-----------------------------------"
+
+
+if ! echo "$build_output" | grep -q "Done"; then
+  echo "Build did not complete successfully (missing 'Done')."
+  exit 1
+fi
+
+echo "-----------------------------------"
 read -p "Commit message: " msg
 
 git add .
 git commit -m "$msg"
 git push
 
-echo "---------- Rebuilding NixOS configuration ----------"
-
+echo "---------- Switching to new configuration ----------"
 sudo nixos-rebuild switch --flake .

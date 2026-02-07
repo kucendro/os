3696ef04c911f3513ee1f4c28d663d191a98f6fe
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-07 21:19:06 +0100

    fix

--- flake.nix
+++ flake.nix
@@ -45,6 +45,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
             home-manager.users.kucendro = import ./users/home.nix;
           }
         ];

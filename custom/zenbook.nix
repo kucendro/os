48d097c0b2105b403e9f98c82e90f0765802dcb0
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-07 19:23:13 +0100

    test fix asus drivers

--- custom/zenbook.nix
+++ custom/zenbook.nix
@@ -7,7 +7,7 @@
     layout = "up5401ea";
     wayland = true;
     runtimeDir = "/run/user/1000/";
-    waylandDisplay = "wayland-0";
+    waylandDisplay = "wayland-1";
     ignoreWaylandDisplayEnv = false;
     config = {
       "activation_time" = "0.3";

ea5e9b996709ecf241cbb40087a3f11df4ab09ba
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-07 12:02:48 +0100

    switched to hyprland test

--- display/gnome.nix
+++ display/gnome.nix
@@ -4,7 +4,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # # Enable the GNOME Desktop Environment.
+  # services.xserver.displayManager.gdm.enable = true;
+  # services.xserver.desktopManager.gnome.enable = true;
 }

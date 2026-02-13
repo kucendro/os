732aefe8e975fdbefe2b60dbb8db1d887c2ac432
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-13 11:26:04 +0100

    added bluetooth

--- config/configuration.nix
+++ config/configuration.nix
@@ -27,6 +27,28 @@
     settings.Resolve.Domains = [ "~work.local" ];
   };
 
+  hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+  settings = {
+    General = {
+      # Shows battery charge of connected devices on supported
+      # Bluetooth adapters. Defaults to 'false'.
+      Experimental = true;
+      # When enabled other devices can connect faster to us, however
+      # the tradeoff is increased power consumption. Defaults to
+      # 'false'.
+      FastConnectable = false;
+    };
+    Policy = {
+      # Enable all controllers when they are found. This includes
+      # adapters present on start as well as adapters that are plugged
+      # in later on. Defaults to 'true'.
+      AutoEnable = true;
+    };
+  };
+};
+
   time.timeZone = "Europe/Prague";
 
   i18n.defaultLocale = "en_US.UTF-8";

e4dd70816dfe63aeb8a964a0b09559f4f8da9987
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-16 14:58:25 +0100

    test zsh

--- terminal/zsh.nix
+++ terminal/zsh.nix
@@ -9,11 +9,11 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
 
-    loginShellInit = ''
-      if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = "1" ]; then
-        start-hyprland
-      fi
-    '';
+    # loginShellInit = ''
+    #   if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" = "1" ]; then
+    #     start-hyprland
+    #   fi
+    # '';
 
     shellAliases = {
       rebuild = "~/nixos/rebuild.sh";

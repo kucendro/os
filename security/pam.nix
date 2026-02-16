f4eb88a521c1c26d2d0fe55d9e9bdd7c681cdb4a
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-16 15:36:40 +0100

    successfully set pam to use howdy

--- security/pam.nix
+++ security/pam.nix
@@ -1,13 +1,6 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  security.pam.services.sudo_local = {
-    text = ''
-      auth      sufficient      pam_python.so /run/current-system/sw/lib/security/howdy/pam.py
-      auth      sufficient      pam_unix.so try_first_pass nullok
-      auth      required        pam_deny.so
-      account   required        pam_unix.so
-      session   required        pam_unix.so
-    '';
-  };
+  # ! This is absolutely not secure, do not use this in your config !!!
+  security.pam.services.sudo.rules.auth.howdy.control = lib.mkForce "sufficient";
 }

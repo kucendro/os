367c5c0d5f155a34faba55b95b9b9bbe11492d2a
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-16 08:25:05 +0100

    added service for mounting discs for nautilus

--- users/home.nix
+++ users/home.nix
@@ -6,11 +6,16 @@
 
   # Noctalia shell configuration
   xdg.configFile = {
-    "noctalia/settings.json".source = config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/noctalia/settings.json";
-    "noctalia/colors.json".source = config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/noctalia/colors.json";
-    "noctalia/plugins.json".source = config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/noctalia/plugins.json";
-    "hypridle/hypridle.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/hypridle/hypridle.conf";
-    "hyprlock/hyprlock.conf".source = config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/hyprlock/hyprlock.conf";
+    "noctalia/settings.json".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/noctalia/settings.json";
+    "noctalia/colors.json".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/noctalia/colors.json";
+    "noctalia/plugins.json".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/noctalia/plugins.json";
+    "hypridle/hypridle.conf".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/hypridle/hypridle.conf";
+    "hyprlock/hyprlock.conf".source =
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/display/hyprlock/hyprlock.conf";
   };
 
   programs.home-manager.enable = true;
@@ -22,6 +27,15 @@
       user = "git";
       identityFile = "/home/kucendro/.ssh/github_ed25519";
       identitiesOnly = true;
+    };
+  };
+
+  services.udiskie = {
+    enable = true;
+    settings = {
+      program_options = {
+        file_manager = "${pkgs.nautilus}/bin/nautilus";
+      };
     };
   };
 

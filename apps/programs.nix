9817a6dacd3989d511b65e2663fe70373e84a572
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-16 14:22:26 +0100

    refactor

--- apps/programs.nix
+++ apps/programs.nix
@@ -1,21 +1,10 @@
 { config, pkgs, ... }:
 
 {
-  # programs.firefox.enable = true;
-  # programs.git.enable = true;
-  # programs.zsh.enable = true;
-  # programs.appimage.enable = true;
-  # programs.appimage.binfmt = true;
-  # programs.evince.enable = true;
-
-
-  programs = [
-    firefox.enable
-    zsh.enable
-    git.enable
-    evince.enable
-    appimage.enable
-    appimage.binfmt
-  ];
-
+  programs.firefox.enable = true;
+  programs.git.enable = true;
+  programs.zsh.enable = true;
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
+  programs.evince.enable = true;
 }

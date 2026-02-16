5ae25cf7ba7003a39e314f3bd533eeca36fbd6c4
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-16 15:52:35 +0100

    test IR

--- security/pam.nix
+++ security/pam.nix
@@ -3,4 +3,7 @@
 {
   # ! This is not secure, do not use this in your config !!!
   security.pam.services.sudo.rules.auth.howdy.control = lib.mkForce "sufficient";
+
+  # ! And this is the absolute shit because i am stupid bitch DO NOT USE THIS JUST COMMENT THIS LINE BEFORE REBUILD !!!
+  security.pam.services.greetd.rules.auth.howdy.control = lib.mkForce "sufficient";
 }

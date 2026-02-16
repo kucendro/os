9aeaf7ee06bbc9e306b91c9d937531fc4060083f
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-16 09:51:46 +0100

    test auth

--- config/configuration.nix
+++ config/configuration.nix
@@ -115,6 +115,18 @@
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.login.enableGnomeKeyring = true;
 
+  # ! Security
+
+  security.pam.services.sudo = {
+    text = ''
+      auth      sufficient      pam_unix.so try_first_pass nullok
+      auth      sufficient      pam_python.so /run/current-system/sw/lib/security/howdy/pam.py
+      auth      required        pam_deny.so
+      account   required        pam_unix.so
+      session   required        pam_unix.so
+    '';
+  };
+
   services.howdy.enable = false;
 
   # Enable the OpenSSH daemon.

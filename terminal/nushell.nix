5f453a625de97ce2e76b575a2c71e7a0b22385fe
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-16 15:04:09 +0100

    nushell

--- terminal/nushell.nix
+++ terminal/nushell.nix
@@ -1,18 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  # users.defaultUserShell = pkgs.zsh;
+  users.defaultUserShell = pkgs.nushell;
 
-  programs.zsh = {
+  programs.nushell = {
     enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
-    syntaxHighlighting.enable = true;
 
     shellAliases = {
       rebuild = "~/nixos/rebuild.sh";
       worklouder = "appimage-run input-0.13.2-Community.AppImage";
       kdrive = "appimage-run kDrive-3.7.9.1-amd64.AppImage";
-    };
-  };
+
+  }
+
 }

3ed7de806a0f349977651bec972211c1bef9b161
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-17 12:46:06 +0100

    script edited

--- security/toggle-password.sh
+++ security/toggle-password.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo -v
 PASSWORD=$(sops --decrypt ~/nixos/secrets/secrets.yaml | yq -r '.password')
-echo -n "$PASSWORD" | wl-copy
-wtype --paste
+wtype "$PASSWORD"
 unset PASSWORD
+sudo -k

1ba8a63b6f548cd16dc5bd4ff351d05a959ff9c7
Ondřej Kučera <ondrej@kucendro.eu>
2026-02-18 09:12:09 +0100

    setup cloudflared

--- vpn/cloudflared.nix
+++ vpn/cloudflared.nix
@@ -1,13 +1,13 @@
 { config, pkgs, ... }:
 
 {
-  # services.cloudflared = {
-  #   enable = true;
-  #   tunnels = {
-  #     "00000000-0000-0000-0000-000000000000" = {
-  #       credentialsFile = "${config.sops.secrets.cloudflared-creds.path}";
-  #       default = "http_status:404";
-  #     };
-  #   };
-  # };
+  services.cloudflared = {
+    enable = true;
+    tunnels = {
+      "ada27b35-266c-47cb-8161-92e83214074d" = {
+        credentialsFile = "${config.sops.secrets.cloudflared.path}";
+        default = "http_status:404";
+      };
+    };
+  };
 }

3f830a8f373a004b8687368c566d07511a2e7dd4
Ondřej Kučera <ondrej@kucendro.eu>
2026-03-02 17:14:11 +0100

    tmux config

--- terminal/tmux.nix
+++ terminal/tmux.nix
@@ -1,17 +1,15 @@
 { pkgs, ... }:
 {
   programs.tmux = {
-  shell = "${pkgs.bash}/bin/zsh";
-  baseIndex = 1;
-  newSession = true;
-  enable = true;
-  escapeTime = 0;
-  secureSocket = false;
-  mouse = true;
-  clock24 = true;
-  historyLimit = 50000;
-  plugins = with pkgs; [
+    baseIndex = 1;
+    newSession = true;
+    enable = true;
+    escapeTime = 0;
+    secureSocket = false;
+    clock24 = true;
+    historyLimit = 50000;
+    plugins = with pkgs; [
       tmuxPlugins.better-mouse-mode
     ];
-}
+  };
 }

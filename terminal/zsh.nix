304272f2dc518e9ccbef7c7668a4ad3908f3e1e6
Ondřej Kučera <ondrej@kucendro.eu>
2026-03-03 18:48:44 +0100

    rem tmux init

--- terminal/zsh.nix
+++ terminal/zsh.nix
@@ -9,13 +9,6 @@
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     histSize = 10000;
-    
-    shellInit = ''
-      if [ -z "$TMUX" ] && [ -n "$DISPLAY" ]; then
-        tmux attach-session -t 1 || tmux new-session -s 1
-      fi
-    '';
-
     shellAliases = {
       rebuild = "~/nixos/rebuild.sh";
       tunnel = "cloudflared tunnel run nixbook";

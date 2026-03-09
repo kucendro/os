18db136415436a59ade46f399bb933602b12a06d
Ondřej Kučera <ondrej@kucendro.eu>
2026-03-09 10:26:44 +0100

    test lazy vim

--- development/neovim.nix
+++ development/neovim.nix
@@ -1,21 +1,34 @@
 { pkgs, ... }:
+let
+  catppuccin = pkgs.vimPlugins.catppuccin-nvim;
+in
 {
   programs.neovim = {
     enable = true;
     plugins = with pkgs.vimPlugins; [
       lazy-nvim
       LazyVim
-      mini-nvim
+      catppuccin-nvim
     ];
     extraLuaConfig = ''
       require("lazy").setup({
         spec = {
           { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+          {
+            "catppuccin/nvim",
+            name = "catppuccin",
+            dir = "${catppuccin}",
+            lazy = false,
+            priority = 1000,
+            opts = { flavour = "macchiato" },
+          },
         },
         performance = {
           reset = false,  -- don't clear the Nix-managed runtimepath
         },
       })
+
+      vim.cmd.colorscheme("catppuccin")
     '';
   };
 }

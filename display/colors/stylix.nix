1c924e42fc45c19294d6c4025e327787e18638e2
Ondřej Kučera <ondrej@kucendro.eu>
2026-03-09 10:15:46 +0100

    cursdor

--- display/colors/stylix.nix
+++ display/colors/stylix.nix
@@ -2,18 +2,18 @@
 {
   stylix = {
     enable = true;
-    # targets = {
-    #   console.enable = false;
-    #   chromium.enable = false;
-    # };re
+    targets = {
+      console.enable = false;
+      chromium.enable = false;
+    };
     # image = ../../wallpaper.jpg;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-    # cursor = {
-    #   package = pkgs.bibata-cursors;
-    #   name = "Bibata-Modern-Ice";
-    #   size = 24;
-    # };
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Ice";
+      size = 24;
+    };
     fonts = {
       serif = {
         package = pkgs.inter;

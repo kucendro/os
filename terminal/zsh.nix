3dcda52d3fcf440be6cb11d945a3b9bd92c02b02
Ondřej Kučera <ondrej@kucendro.eu>
2026-03-20 14:58:33 +0100

    zsh

--- terminal/zsh.nix
+++ terminal/zsh.nix
@@ -13,7 +13,8 @@
     shellAliases = {
       rebuild = "~/nixos/rebuild.sh";
       ls = "lsd";
-      cl = "clear";
+      c = "clear";
+      e = "exit";
       tunnel = "cloudflared tunnel run nixbook";
       wg-prod-up = "nmcli connection up prod";
       wg-prod-down = "nmcli connection down prod";

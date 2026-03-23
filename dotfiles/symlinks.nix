6ad314f4c30b2a8cdc417259a01d5ef9efe58386
Ondřej Kučera <ondrej@kucendro.eu>
2026-03-23 20:07:55 +0100

    fixed symlinks

--- dotfiles/symlinks.nix
+++ dotfiles/symlinks.nix
@@ -2,22 +2,22 @@
 {
   xdg.configFile = {
     "noctalia/settings.json".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/noctalia/settings.json";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/noctalia/settings.json";
     "noctalia/colors.json".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/noctalia/colors.json";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/noctalia/colors.json";
     "noctalia/plugins.json".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/noctalia/plugins.json";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/noctalia/plugins.json";
     "hypridle/hypridle.conf".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/hypridle.conf";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/hypridle.conf";
     "hyprlock/hyprlock.conf".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/hyprlock.conf";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/hyprlock.conf";
     "hypr/hyprland.conf".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/hyprland.conf";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/hyprland.conf";
     "nvim/init.lua".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/nvim/init.lua";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/nvim/init.lua";
     "nvim/lazy-lock.json".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/nvim/lazy-lock.json";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/nvim/lazy-lock.json";
     "nvim/lazyvim.json".source =
-      config.lib.file.mkOutOfStoreSymlink "home/kucendro/nixos/dotfiles/nvim/lazyvim.json";
+      config.lib.file.mkOutOfStoreSymlink "/home/kucendro/nixos/dotfiles/nvim/lazyvim.json";
   };
 }

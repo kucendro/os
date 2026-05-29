5b43a1595bbaba9de2530246a70bfc5595757c50
Ondřej Kučera <ondrej@kucendro.eu>
2026-05-29 18:01:03 +0200

    fix symlinks

--- dotfiles/symlinks.nix
+++ dotfiles/symlinks.nix
@@ -10,18 +10,20 @@ let
   isNixbook = osConfig.networking.hostName == "nixbook";
   hasDotfiles = !(osConfig.boot.isContainer or false);
   mkSymlink = path: config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/${path}";
-  noctaliaPkg = inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in
 {
-  xdg.configFile = {
-    "noctalia/settings.json".source = lib.mkIf isNixbook (
-      lib.mkForce (mkSymlink "noctalia/settings.json")
-    );
-    "noctalia/plugins.json".source = lib.mkIf isNixbook (mkSymlink "noctalia/plugins.json");
-    "nvim/lazy-lock.json".source = lib.mkIf hasDotfiles (mkSymlink "nvim/lazy-lock.json");
-    "nvim/lazyvim.json".source = lib.mkIf hasDotfiles (mkSymlink "nvim/lazyvim.json");
-  };
+  xdg.configFile =
+    (lib.optionalAttrs isNixbook {
+      "noctalia/settings.json".source = lib.mkForce (mkSymlink "noctalia/settings.json");
+      "noctalia/plugins.json".source = mkSymlink "noctalia/plugins.json";
+    })
+    // (lib.optionalAttrs hasDotfiles {
+      "nvim/lazy-lock.json".source = mkSymlink "nvim/lazy-lock.json";
+      "nvim/lazyvim.json".source = mkSymlink "nvim/lazyvim.json";
+    });
 
-  xdg.dataFile."applications/dev.noctalia.noctalia-qs.desktop".source =
-    lib.mkIf isNixbook "${noctaliaPkg}/share/applications/dev.noctalia.noctalia-qs.desktop";
+  xdg.dataFile = lib.optionalAttrs isNixbook {
+    "applications/dev.noctalia.noctalia-qs.desktop".source =
+      "${inputs.noctalia.packages.${pkgs.stdenv.hostPlatform.system}.default}/share/applications/dev.noctalia.noctalia-qs.desktop";
+  };
 }

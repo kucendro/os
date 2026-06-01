41eca172aa3d5996806390ffa86d891d38ee23a5
Ondřej Kučera <ondrej@kucendro.eu>
2026-06-01 23:35:36 +0200

    headscale reenable

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -1,10 +1,9 @@
-{ ... }:
+{ config, ... }:
 
 {
   services.tailscale = {
     enable = true;
-    # authKeyFile re-enabled once edge.kucendro.dev is reachable + ACME issued.
-    # In the meantime, run on each host once: sudo tailscale up --login-server=https://edge.kucendro.dev
+    authKeyFile = config.sops.secrets.tailscale-authkey.path;
     extraUpFlags = [
       "--login-server=https://edge.kucendro.dev"
     ];

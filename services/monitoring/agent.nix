828d72e47136f766c6763b1c56ad1badf6582c0e
Ondřej Kučera <ondrej@kucendro.eu>
2026-06-03 19:52:38 +0200

    revert the ssh auth

--- services/monitoring/agent.nix
+++ services/monitoring/agent.nix
@@ -3,6 +3,9 @@
 {
   services.beszel.agent = {
     enable = true;
+    openFirewall = false;
     environmentFile = config.sops.templates."beszel-agent-env".path;
   };
+
+  networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 45876 ];
 }

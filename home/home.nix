3b3eda836615e4fb623271c8028a3e9dc20e0c1a
Ondřej Kučera <ondrej@kucendro.eu>
2026-07-03 16:20:41 +0200

    nginx no limit

--- home/home.nix
+++ home/home.nix
@@ -52,7 +52,10 @@
     settings = {
       auto-optimise-store = true;
       warn-dirty = false;
-      allowed-users = [ me.name ];
+      allowed-users = [
+        me.name
+        "root"
+      ];
     };
     gc = {
       automatic = true;
@@ -84,38 +87,38 @@
       };
     };
 
-    qutebrowser = lib.mkIf (profile == "desktop") {
-      enable = true;
-      searchEngines = {
-        DEFAULT = "https://duckduckgo.com/?q={}";
-        g = "https://www.google.com/search?hl=en&q={}";
-        n = "https://search.nixos.org/packages?channel=25.11&query={}";
-        w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
-        v = "https://vault.home.kucendro.dev/#/vault?search={}";
-      };
-      settings = {
-        "colors.webpage.darkmode.enabled" = true;
-        "content.cookies.accept" = "no-3rdparty";
-        "content.geolocation" = "ask";
-        "content.notifications.enabled" = "ask";
-        "content.autoplay" = true;
-        "downloads.location.directory" = "~/Downloads";
-        "downloads.location.prompt" = false;
-        "tabs.show" = "multiple";
-        "statusbar.show" = "in-mode";
-        "scrolling.smooth" = true;
-        "fonts.default_family" = "monospace";
-        "fonts.default_size" = "11pt";
-        "auto_save.session" = true;
-        "session.lazy_restore" = true;
-      };
-      keyBindings = {
-        normal = {
-          "J" = "tab-prev";
-          "K" = "tab-next";
-        };
-      };
-    };
+    # qutebrowser = lib.mkIf (profile == "desktop") {
+    #   enable = true;
+    #   searchEngines = {
+    #     DEFAULT = "https://duckduckgo.com/?q={}";
+    #     g = "https://www.google.com/search?hl=en&q={}";
+    #     n = "https://search.nixos.org/packages?channel=25.11&query={}";
+    #     w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
+    #     v = "https://vault.home.kucendro.dev/#/vault?search={}";
+    #   };
+    #   settings = {
+    #     "colors.webpage.darkmode.enabled" = true;
+    #     "content.cookies.accept" = "no-3rdparty";
+    #     "content.geolocation" = "ask";
+    #     "content.notifications.enabled" = "ask";
+    #     "content.autoplay" = true;
+    #     "downloads.location.directory" = "~/Downloads";
+    #     "downloads.location.prompt" = false;
+    #     "tabs.show" = "multiple";
+    #     "statusbar.show" = "in-mode";
+    #     "scrolling.smooth" = true;
+    #     "fonts.default_family" = "monospace";
+    #     "fonts.default_size" = "11pt";
+    #     "auto_save.session" = true;
+    #     "session.lazy_restore" = true;
+    #   };
+    #   keyBindings = {
+    #     normal = {
+    #       "J" = "tab-prev";
+    #       "K" = "tab-next";
+    #     };
+    #   };
+    # };
 
     zsh = {
       enable = true;
@@ -136,6 +139,7 @@
         ga = "git add .";
         gc = "git commit -m";
         gp = "git push";
+        gpl = "git pull";
         gd = "git diff";
         link-phone = "adb tcpip 5555";
 
--- services/mesh/proxied/services.nix
+++ services/mesh/proxied/services.nix
@@ -22,11 +22,16 @@ let
     listenAddresses = [ tailnetIP ];
     useACMEHost = homeDomain;
     forceSSL = true;
+    extraConfig = ''
+      client_max_body_size 0; # no upload size limit
+    '';
     locations."/" = {
       proxyPass = "http://$upstream_${name}";
       proxyWebsockets = true;
       extraConfig = ''
         set $upstream_${name} ${hostPort};
+        proxy_read_timeout 600s;
+        proxy_send_timeout 600s;
       '';
     };
   };

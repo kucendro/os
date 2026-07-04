8d9a10c015a11ecb64fc52c42ec3e49781bc5c81
Ondřej Kučera <ondrej@kucendro.eu>
2026-07-04 15:51:09 +0200

    grafana alerting

--- services/monitoring/blackbox.nix
+++ services/monitoring/blackbox.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   pkgs,
   ...
@@ -92,5 +93,115 @@ in
         options.path = ./blackbox-dashboard.json;
       }
     ];
+    alerting.contactPoints.settings = {
+      apiVersion = 1;
+      contactPoints = [
+        {
+          orgId = 1;
+          name = "Telegram bot";
+          receivers = [
+            {
+              uid = "afr3gp4x53myoc";
+              type = "telegram";
+              settings = {
+                bottoken = "$__file{${config.sops.secrets.grafana-telegram-bottoken.path}}";
+                chatid = "8323605619";
+                disable_notification = false;
+                disable_web_page_preview = false;
+                message = ''{{ template "telegram.default.message" . }}'';
+                protect_content = false;
+              };
+              disableResolveMessage = true;
+            }
+          ];
+        }
+      ];
+    };
+    alerting.rules.settings = {
+      apiVersion = 1;
+      groups = [
+        {
+          orgId = 1;
+          name = "endpoints";
+          folder = "endpoints";
+          interval = "1m";
+          rules = [
+            {
+              uid = "ffr3i429c8c8wa";
+              title = "Endpoints";
+              condition = "C";
+              data = [
+                {
+                  refId = "A";
+                  relativeTimeRange = {
+                    from = 600;
+                    to = 0;
+                  };
+                  datasourceUid = "prometheus";
+                  model = {
+                    editorMode = "builder";
+                    exemplar = false;
+                    expr = "group by(instance) (probe_success == 0)";
+                    instant = true;
+                    intervalMs = 1000;
+                    legendFormat = "__auto";
+                    maxDataPoints = 43200;
+                    range = false;
+                    refId = "A";
+                  };
+                }
+                {
+                  refId = "C";
+                  queryType = "expression";
+                  relativeTimeRange = {
+                    from = 0;
+                    to = 0;
+                  };
+                  datasourceUid = "__expr__";
+                  model = {
+                    conditions = [
+                      {
+                        evaluator = {
+                          params = [ 0 ];
+                          type = "gt";
+                        };
+                        operator.type = "and";
+                        query.params = [ "C" ];
+                        reducer = {
+                          params = [ ];
+                          type = "last";
+                        };
+                        type = "query";
+                      }
+                    ];
+                    datasource = {
+                      type = "__expr__";
+                      uid = "__expr__";
+                    };
+                    expression = "A";
+                    intervalMs = 1000;
+                    maxDataPoints = 43200;
+                    refId = "C";
+                    type = "threshold";
+                  };
+                }
+              ];
+              dashboardUid = "home-endpoints";
+              panelId = 2;
+              noDataState = "NoData";
+              execErrState = "Error";
+              for = "1m";
+              annotations = {
+                __dashboardUid__ = "home-endpoints";
+                __panelId__ = "2";
+              };
+              labels = { };
+              isPaused = false;
+              notification_settings.receiver = "Telegram bot";
+            }
+          ];
+        }
+      ];
+    };
   };
 }

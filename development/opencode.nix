7aae05984ce87ec86ca44ac4c50002fe69443e49
Ondřej Kučera <ondrej@kucendro.eu>
2026-08-05 15:48:12 +0200

    resend mcp to opencode

--- development/opencode.nix
+++ development/opencode.nix
@@ -9,13 +9,19 @@
   programs.opencode = {
     enable = true;
     package = pkgs.opencode;
-
     settings = lib.optionalAttrs (profile == "desktop") {
-      mcp.homelab = {
-        type = "remote";
-        url = "https://mcp.home.kucendro.dev/mcp";
-        enabled = true;
-        headers.Authorization = "Bearer {file:/run/secrets/gitea-mcp-token}";
+      mcp = {
+        homelab = {
+          type = "remote";
+          url = "https://mcp.home.kucendro.dev/mcp";
+          enabled = true;
+          headers.Authorization = "Bearer {file:/run/secrets/gitea-mcp-token}";
+        };
+        resend = {
+          type = "remote";
+          url = "https://mcp.resend.com/mcp";
+          enabled = true;
+        };
       };
     };
   };

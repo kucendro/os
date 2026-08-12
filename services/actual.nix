761415a46d3a1cec96f8e1cba99bdfb203ba00d1
Ondřej Kučera <ondrej@kucendro.eu>
2026-08-12 15:54:55 +0200

    fix

--- services/actual.nix
+++ services/actual.nix
@@ -6,12 +6,19 @@ in
 {
   services.actual = {
     enable = true;
-    settings = {
-      inherit port;
-      dataDir = "/mnt/data/actual";
-    };
+    settings.port = port;
   };
 
-  systemd.services.actual.unitConfig.RequiresMountsFor = [ "/mnt/data" ];
+  systemd.tmpfiles.rules = [
+    "d /mnt/data/actual 0700 root root -"
+  ];
+
+  fileSystems."/var/lib/actual" = {
+    device = "/mnt/data/actual";
+    fsType = "none";
+    options = [ "bind" ];
+  };
+
+  systemd.services.actual.unitConfig.RequiresMountsFor = [ "/var/lib/actual" ];
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ port ];
 }
--- services/monitoring/hub.nix
+++ services/monitoring/hub.nix
@@ -1,8 +1,6 @@
 { ... }:
 
 {
-  # The beszel hub itself. Its public ingress (monitoring.home.kucendro.dev) is
-  # declared with the other *.home reverse proxies in ../mesh/proxied/services.nix.
   services.beszel.hub = {
     enable = true;
     host = "127.0.0.1";

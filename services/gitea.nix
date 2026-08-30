e3eeda8ca4d5177868df68e8c7d66947f82c5a77
Ondřej Kučera <kucera@deepvision.cz>
2026-08-30 21:41:32 +0200

    fix nas

--- services/gitea.nix
+++ services/gitea.nix
@@ -73,7 +73,7 @@ in
   system.activationScripts.gitea-runner-ssh = ''
     mkdir -p /var/lib/gitea-runner/.ssh
     chmod 700 /var/lib/gitea-runner/.ssh
-    printf 'Host ${sshDomain}\n  IdentityFile /var/lib/gitea-runner/.ssh/id_ed25519\n  StrictHostKeyChecking accept-new\n  Port ${toString sshPort}\n' > /var/lib/gitea-runner/.ssh/config
+    printf 'Host nas ${sshDomain}\n  IdentityFile /var/lib/gitea-runner/.ssh/id_ed25519\n  StrictHostKeyChecking accept-new\n  Port ${toString sshPort}\n' > /var/lib/gitea-runner/.ssh/config
     chmod 600 /var/lib/gitea-runner/.ssh/config
     chown gitea-runner:gitea-runner /var/lib/gitea-runner/.ssh/config
   '';

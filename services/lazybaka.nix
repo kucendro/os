b3fd2449bbe49e9eab8813460a5f4f493da6ae88
Ondřej Kučera <kucera@deepvision.cz>
2026-08-30 22:14:01 +0200

    less stress on school server

--- services/lazybaka.nix
+++ services/lazybaka.nix
@@ -14,6 +14,6 @@
     ];
     calendarId = "8a589d924652a0a312c6ba832efcbb187f455fe945246a5f406b587fa2ff3521@group.calendar.google.com";
     serviceAccountKeyFile = config.sops.secrets.bakasync-service-account.path;
-    interval = "1min";
+    interval = "5min";
   };
 }

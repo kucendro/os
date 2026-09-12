bbc6e2adb3e05a23c55a1a96834e2c538f3d0198
Ondřej Kučera <kucera@deepvision.cz>
2026-09-13 00:51:17 +0200

    Matter devices server for teh home assistant.

--- services/dockerized/home-assistant.nix
+++ services/dockerized/home-assistant.nix
@@ -3,12 +3,11 @@
 let
   port = 8123;
   reverseProxyIP = "100.64.0.1";
+  lanInterface = "enp1s0";
 
   configYaml = pkgs.writeText "home-assistant-configuration.yaml" ''
-    # Loads default set of integrations. Do not remove.
     default_config:
 
-    # Load frontend themes from the themes folder
     frontend:
       themes: !include_dir_merge_named themes
 
@@ -27,6 +26,7 @@ in
     backend = "docker";
 
     #: unit home-assistant
+    #: -> nas/matter-server matter :5580
     containers.home-assistant = {
       image = "ghcr.io/home-assistant/home-assistant:stable";
       volumes = [
@@ -41,9 +41,32 @@ in
         "--network=host"
       ];
     };
+
+    #: unit matter-server
+    containers.matter-server = {
+      image = "ghcr.io/matter-js/matterjs-server:stable";
+      volumes = [ "/mnt/data/matter-server:/data" ];
+
+      environment = {
+        TZ = "Europe/Prague";
+        PRIMARY_INTERFACE = lanInterface;
+      };
+
+      extraOptions = [
+        "--network=host"
+        "--read-only"
+      ];
+    };
   };
 
-  systemd.services.docker-home-assistant.unitConfig.RequiresMountsFor = [ "/mnt/data" ];
+  systemd.services.docker-home-assistant = {
+    unitConfig.RequiresMountsFor = [ "/mnt/data" ];
+    after = [ "docker-matter-server.service" ];
+  };
+  systemd.services.docker-matter-server.unitConfig.RequiresMountsFor = [ "/mnt/data" ];
+
+  boot.kernelModules = [ "nf_conntrack" ];
+  boot.kernel.sysctl."net.netfilter.nf_conntrack_udp_timeout_stream" = 3600;
 
   systemd.tmpfiles.rules = [
     "d /mnt/data/home-assistant 0700 root root -"
@@ -51,18 +74,22 @@ in
     "f /mnt/data/home-assistant/automations.yaml 0644 root root - []"
     "f /mnt/data/home-assistant/scripts.yaml 0644 root root -"
     "f /mnt/data/home-assistant/scenes.yaml 0644 root root -"
+    "d /mnt/data/matter-server 0700 1000 1000 -"
   ];
 
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ port ];
 
-  networking.firewall.interfaces."enp1s0".allowedTCPPorts = [
-    9999
-    80
-    443
-  ];
-  networking.firewall.interfaces."enp1s0".allowedUDPPorts = [
-    9999
-    20002
-    1900
-  ];
+  networking.firewall.interfaces.${lanInterface} = {
+    allowedTCPPorts = [
+      9999
+      80
+      443
+    ];
+    allowedUDPPorts = [
+      9999
+      20002
+      1900
+      5353
+    ];
+  };
 }

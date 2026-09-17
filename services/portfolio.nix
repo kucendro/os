5bf08a05a9797cf2b267c5e670055c6ac232659f
Ondřej Kučera <kucera@deepvision.cz>
2026-09-17 23:31:47 +0200

    Portfolio selfhosted

--- services/portfolio.nix
+++ services/portfolio.nix
@@ -4,7 +4,6 @@
   imports = [ inputs.portfolio.nixosModules.kucendro ];
 
   #: unit nginx
-  #: unit kucendro-github
   services.kucendro = {
     enable = true;
     github.tokenFile = config.sops.secrets.kucendro-github-token.path;

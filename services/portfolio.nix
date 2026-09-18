7577beb242283ee0e3acfd95116669a92bf8b431
Ondřej Kučera <kucera@deepvision.cz>
2026-09-18 18:15:29 +0200

    portfolio

--- services/portfolio.nix
+++ services/portfolio.nix
@@ -7,6 +7,7 @@
   services.kucendro = {
     enable = true;
     github.tokenFile = config.sops.secrets.kucendro-github-token.path;
+    live.user = "gitea-runner";
   };
 
   networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 80 ];
